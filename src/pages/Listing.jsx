67d51d6fc5cce3468404c54fb248811615c0c66f
malkochilal <119036086+malkochilal@users.noreply.github.com>
2023-05-17 11:40:12 +0300

    add share copy functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -12,14 +12,14 @@ import SwiperCore, {
   Pagination,
 } from "swiper";
 import "swiper/css/bundle";
-// import {
-//   FaShare,
-//   FaMapMarkerAlt,
-//   FaBed,
-//   FaBath,
-//   FaParking,
-//   FaChair,
-// } from "react-icons/fa";
+import {
+  FaShare,
+  //   FaMapMarkerAlt,
+  //   FaBed,
+  //   FaBath,
+  //   FaParking,
+  //   FaChair,
+} from "react-icons/fa";
 import { getAuth } from "firebase/auth";
 // import Contact from "../components/Contact";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
@@ -68,6 +68,27 @@ export default function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div
+        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex 
+      justify-center items-center "
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <FaShare className="text-lg text-slate-500" />
+      </div>
+      {shareLinkCopied && (
+        <p
+          className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400
+      rounded-md bg-white z-10 p-2"
+        >
+          Link Copied
+        </p>
+      )}
     </main>
   );
 }
